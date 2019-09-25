e91366b3c16926f2aa85c84f0a8123cf3c9fb6c3
fengfeng8867dd@hotmail.com <fengfeng8867dd@hotmail.com>
2019-09-25 07:53:01 -0600

    Core feature throw and recall done

---
+++ Assets/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x + 5, 3, transform.position.z);
+    }
+}
