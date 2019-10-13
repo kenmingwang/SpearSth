0183ff99ddfe9cd4dda77d2d64ee8655eabd82dd
Cameron Levezu <levezuc@gmail.com>
2019-10-13 14:54:45 -0600

    Added Walker script
    Patroling works, but collision does not

---
+++ Assets/Scripts/EnemyWalker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalker : MonoBehaviour
+{
+    public float speed;
+
+    private bool movingRight = true;
+
+    public Transform groundDectecton;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDectecton.position, Vector2.down, 1f);
+
+        if(groundInfo.collider == false)
+        {
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            } else
+            {
+                transform.eulerAngles = new Vector3(0, -0, 0);
+                movingRight = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Spear")
+        {
+            Debug.Log("hit");
+            Destroy(gameObject);
+        }
+    }
+}
