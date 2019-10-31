2aed1c2c050f7e8ed0a8a68acceb076c57ec2322
Cameron Levezu <levezuc@gmail.com>
2019-10-31 12:05:15 -0600

    Finished adding cameras to lvl0

--- Assets/Scripts/CameraWaypoint.cs
+++ Assets/Scripts/CameraWaypoint.cs
@@ -13,6 +13,11 @@ public class CameraWaypoint: MonoBehaviour
     void Start()
     {
         camera = GameObject.Find("MegaCamera");
+
+        if(nextCameraPos.tag == "CameraPosition")
+        {
+            Debug.Log("tagem and bag em");
+        }
     }
 
     // Update is called once per frame
@@ -26,14 +31,17 @@ public class CameraWaypoint: MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("passed");
-            if (camera.transform.position.x == prevCameraPos.transform.position.x && camera.transform.position.y == prevCameraPos.transform.position.y)
+            //These are some pretty beefy if statments. The movement of the camera is not perfect, so the if statements give a little wiggle room for camera if it misses slightly
+            if ((camera.transform.position.x <= prevCameraPos.transform.position.x + 0.1f && camera.transform.position.x >= prevCameraPos.transform.position.x - 0.1f) 
+                && camera.transform.position.y <= prevCameraPos.transform.position.y + 0.1f && camera.transform.position.y >= prevCameraPos.transform.position.y - 0.1f)
             {
-                Debug.Log("dab");
+                Debug.Log("go foreward");
                 float newX = prevCameraPos.transform.position.x - nextCameraPos.transform.position.x;
                 float newY = prevCameraPos.transform.position.y - nextCameraPos.transform.position.y;
                 camera.transform.Translate(-newX, -newY, 0.0f);
             }
-            else
+            else if ((camera.transform.position.x <= nextCameraPos.transform.position.x + 0.1f && camera.transform.position.x >= nextCameraPos.transform.position.x - 0.1f)
+                && camera.transform.position.y <= nextCameraPos.transform.position.y + 0.1f && camera.transform.position.y >= nextCameraPos.transform.position.y - 0.1f)
             {
                 Debug.Log("go back");
                 float newX = prevCameraPos.transform.position.x - nextCameraPos.transform.position.x;
