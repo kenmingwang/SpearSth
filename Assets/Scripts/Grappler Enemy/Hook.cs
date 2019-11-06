7e7e8ea6f1e55b6a2fa3fc4687730ff7993681e4
Cameron Levezu <levezuc@gmail.com>
2019-11-05 23:02:59 -0700

    I cant even do detection right :(

--- Assets/Scripts/Grappler Enemy/Hook.cs
+++ Assets/Scripts/Grappler Enemy/Hook.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-
+    [Range(1, 50)]
     public float hookSpeed;
+
+    [Range(1, 50)]
+    public float recallSpeed;
+
+    private bool hasCatch = false;
+
+    private Vector2 velocity;
+
+    Vector2 pos;
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 pos = transform.position;
+        pos = transform.position;
     }
 
     private void Awake()
     {
-        Debug.Log("Kill me");
-        //transform.position();
+        Debug.Log("Device fired");
+        
+        velocity.y -= hookSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("attacked");
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("attacked ground");
+        } 
+    }
+
+    private void Update()
+    {
+        transform.Translate(velocity * Time.deltaTime);
     }
 }
