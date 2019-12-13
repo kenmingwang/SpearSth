3e2231fb64411768f9f68405a741b7f81590c604
kenmingwang <fengfeng8867dd@hotmail.com>
2019-12-13 11:18:07 +0800

    Fix git

--- Assets/Scripts/CasterEnemyBtn.cs
+++ Assets/Scripts/CasterEnemyBtn.cs
@@ -15,6 +15,8 @@ public class CasterEnemyBtn : MonoBehaviour
     public Animation anim;
     public Material hitShader;
     public Material normalShader;
+    public Sprite BtnClosed;
+    private Sprite BtnOpen;
 
     private Renderer rend_one;
     private Renderer rend_two;
@@ -34,7 +36,8 @@ public class CasterEnemyBtn : MonoBehaviour
         countDown = 10f;
         spwanCountDown = 2f;
         castCounDown = 3f;
-        
+        BtnOpen = btn_one.GetComponent<SpriteRenderer>().sprite;
+
     }
 
     // Update is called once per frame
@@ -62,14 +65,14 @@ public class CasterEnemyBtn : MonoBehaviour
         if (btn == btn_one && !btn_one_hit)
         {
             btn_one_hit = true;
-            rend_one.material = hitShader;
+            btn_one.GetComponent<SpriteRenderer>().sprite = BtnClosed;
             hit = true;
             counter++;
-        }  
-        else if(btn == btn_two && !btn_two_hit)
+        }
+        else if (btn == btn_two && !btn_two_hit)
         {
             btn_two_hit = true;
-            rend_two.material = hitShader;
+            btn_two.GetComponent<SpriteRenderer>().sprite = BtnClosed;
             hit = true;
             counter++;
         }
@@ -78,7 +81,7 @@ public class CasterEnemyBtn : MonoBehaviour
     private void CountDownStart()
     {
         countDown -= Time.deltaTime;
-        if(countDown < 0)
+        if (countDown < 0)
         {
             countDown = 10f;
             counter = 0;
@@ -88,8 +91,8 @@ public class CasterEnemyBtn : MonoBehaviour
 
     private void ResetBtn()
     {
-        rend_one.material = normalShader;
-        rend_two.material = normalShader;
+        btn_one.GetComponent<SpriteRenderer>().sprite = BtnOpen;
+        btn_two.GetComponent<SpriteRenderer>().sprite = BtnOpen;
         btn_one_hit = false;
         btn_two_hit = false;
         countDown = 5f;
@@ -100,7 +103,7 @@ public class CasterEnemyBtn : MonoBehaviour
     private void Cast()
     {
         castCounDown -= Time.deltaTime;
-        if(castCounDown < 0)
+        if (castCounDown < 0)
         {
             ani.SetTrigger("Cast");
             castCounDown = 13f;
@@ -112,13 +115,13 @@ public class CasterEnemyBtn : MonoBehaviour
     private void SpawnEnemy()
     {
         spwanCountDown -= Time.deltaTime;
-        if(spwanCountDown < 0)
+        if (spwanCountDown < 0)
         {
             spwanCountDown = 2f;
             Vector3 pos = new Vector3(SpawnDoor.transform.position.x, SpawnDoor.transform.position.y + 0.1f, SpawnDoor.transform.position.z);
-            
+
             Instantiate(Enemy, pos, new Quaternion());
-           
+
             if (++spawnCounter > 5)
             {
                 casting = false;
